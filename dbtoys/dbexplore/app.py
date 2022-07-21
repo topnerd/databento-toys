d322fdfe2b3281785f05d9920eb454a88c668f91
Nick Macholl <nickmacholl@gmail.com>
2022-07-21 11:20:08 -0700

    Refactored command parsers out of app.py

--- dbtoys/dbexplore/app.py
+++ dbtoys/dbexplore/app.py
@@ -5,33 +5,21 @@ import logging.config
 import sys
 from pprint import pformat
 from typing import Callable
-from typing import Tuple
 
 import cmd2
 import databento
 import humanize
-import pandas
 from colorama import Fore
-from databento.common.enums import Compression
-from databento.common.enums import Dataset
-from databento.common.enums import Encoding
-from databento.common.enums import FeedMode
-from databento.common.enums import Schema
 from databento.historical.error import BentoError
 from tabulate import tabulate
 
 import dbtoys.utilities.key
 import dbtoys.utilities.logging
 import dbtoys.utilities.parser
+from dbtoys.dbexplore import command_parsers
 
 _LOG = logging.getLogger()
 _PROG = "dbexplore"
-
-KNOWN_COMPRESSIONS: Tuple[str, ...] = tuple(x.value for x in Compression)
-KNOWN_DATASETS: Tuple[str, ...] = tuple(x.value for x in Dataset)
-KNOWN_ENCODINGS: Tuple[str, ...] = tuple(x.value for x in Encoding)
-KNOWN_FEED_MODES: Tuple[str, ...] = tuple(x.value for x in FeedMode)
-KNOWN_SCHEMAS: Tuple[str, ...] = tuple(x.value for x in Schema)
 
 
 def main(cantrip: str = "", verbose: bool = False) -> int:
@@ -77,217 +65,6 @@ def main(cantrip: str = "", verbose: bool = False) -> int:
         raise exc
     else:
         return 0
-
-
-# Command Parsers
-_parse_get_billable_size: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_get_billable_size.add_argument(
-    "dataset",
-    choices=KNOWN_DATASETS,
-    type=str,
-    help="the target dataset",
-)
-_parse_get_billable_size.add_argument(
-    "symbols", type=str, help="one or more symbols separated by commas"
-)
-_parse_get_billable_size.add_argument(
-    "schema",
-    choices=KNOWN_SCHEMAS,
-    type=str,
-    help="a data schema",
-)
-_parse_get_billable_size.add_argument(
-    "encoding",
-    choices=KNOWN_ENCODINGS,
-    type=str,
-    help="a data encoding",
-)
-_parse_get_billable_size.add_argument(
-    "--start",
-    "-s",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the earlierst date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-_parse_get_billable_size.add_argument(
-    "--end",
-    "-e",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the latest date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-
-_parse_get_cost: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_get_cost.add_argument(
-    "dataset",
-    choices=KNOWN_DATASETS,
-    type=str,
-    help="the target dataset",
-)
-_parse_get_cost.add_argument(
-    "symbols", type=str, help="one or more symbols separated by commas"
-)
-_parse_get_cost.add_argument(
-    "schema",
-    choices=KNOWN_SCHEMAS,
-    type=str,
-    help="a data schema",
-)
-_parse_get_cost.add_argument(
-    "encoding",
-    choices=KNOWN_ENCODINGS,
-    type=str,
-    nargs="?",
-    help="a data encoding",
-    const=None,
-)
-_parse_get_cost.add_argument(
-    "compression",
-    choices=KNOWN_COMPRESSIONS,
-    type=str,
-    nargs="?",
-    help="a data compression",
-    const=None,
-)
-_parse_get_cost.add_argument(
-    "--start",
-    "-s",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the earlierst date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-_parse_get_cost.add_argument(
-    "--end",
-    "-e",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the latest date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-
-_parse_get_shape: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_get_shape.add_argument(
-    "dataset",
-    choices=KNOWN_DATASETS,
-    type=str,
-    help="the target dataset",
-)
-_parse_get_shape.add_argument(
-    "symbols", type=str, help="one or more symbols separated by commas"
-)
-_parse_get_shape.add_argument(
-    "schema",
-    choices=KNOWN_SCHEMAS,
-    type=str,
-    help="a data schema",
-)
-_parse_get_shape.add_argument(
-    "--start",
-    "-s",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the earlierst date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-_parse_get_shape.add_argument(
-    "--end",
-    "-e",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the latest date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-
-_parse_list_datasets: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_list_datasets.add_argument(
-    "--start",
-    "-s",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the earlierst date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-_parse_list_datasets.add_argument(
-    "--end",
-    "-e",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the latest date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-
-_parse_list_fields: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_list_fields.add_argument(
-    "dataset",
-    choices=KNOWN_DATASETS,
-    type=str,
-    help="the target dataset",
-)
-_parse_list_fields.add_argument(
-    "schema",
-    choices=KNOWN_SCHEMAS,
-    type=str,
-    help="a data schema",
-)
-_parse_list_fields.add_argument(
-    "encoding",
-    choices=KNOWN_ENCODINGS,
-    type=str,
-    nargs="?",
-    help="a data encoding",
-    const=None,
-)
-
-_parse_list_schemas: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_list_schemas.add_argument(
-    "dataset",
-    choices=KNOWN_DATASETS,
-    type=str,
-    help="the target dataset",
-)
-_parse_list_schemas.add_argument(
-    "--start",
-    "-s",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the earlierst date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-_parse_list_schemas.add_argument(
-    "--end",
-    "-e",
-    type=pandas.Timestamp.fromisoformat,
-    metavar="YYYY-MM-DDTHHMMSS.MMM",
-    help="the latest date in ISO 8601 format",
-    default=pandas.Timestamp.today().date(),
-)
-
-_parse_list_unit_prices: cmd2.Cmd2ArgumentParser = cmd2.Cmd2ArgumentParser()
-_parse_list_unit_prices.add_argument(
-    "dataset",
-    choices=KNOWN_DATASETS,
-    type=str,
-    help="the target dataset",
-)
-_parse_list_unit_prices.add_argument(
-    "mode",
-    choices=KNOWN_FEED_MODES,
-    type=str,
-    nargs="?",
-    help="a feed mode",
-    default=None,
-)
-_parse_list_unit_prices.add_argument(
-    "schema",
-    choices=KNOWN_SCHEMAS,
-    type=str,
-    nargs="?",
-    help="a data schema",
-    default=None,
-)
 
 
 def log_command(func: Callable) -> Callable:
@@ -336,7 +113,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_get_billable_size)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_get_billable_size)  # type: ignore
     def do_get_billable_size(self, args):
         """Gets the size in bytes of timeseries data."""
         try:
@@ -357,7 +134,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_get_cost)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_get_cost)  # type: ignore
     def do_get_cost(self, args):
         """Gets the cost of timeseries data."""
         try:
@@ -376,7 +153,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_get_shape)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_get_shape)  # type: ignore
     def do_get_shape(self, args):
         """Gets the dimensions of timeseries data."""
         try:
@@ -407,7 +184,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_list_datasets)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_list_datasets)  # type: ignore
     def do_list_datasets(self, args):
         """List all datasets."""
         try:
@@ -435,7 +212,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_list_fields)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_list_fields)  # type: ignore
     def do_list_fields(self, args):
         """List all fields from the given dataset and schema."""
         try:
@@ -466,7 +243,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_list_schemas)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_list_schemas)  # type: ignore
     def do_list_schemas(self, args):
         """List all available schemas for a data set within the given start and end dates."""
         try:
@@ -483,7 +260,7 @@ class DataBentoExplorer(cmd2.Cmd):
 
     @log_command
     @cmd2.with_category(METADATA_COMMANDS)
-    @cmd2.with_argparser(_parse_list_unit_prices)  # type: ignore
+    @cmd2.with_argparser(command_parsers._parse_list_unit_prices)  # type: ignore
     def do_list_unit_prices(self, args):
         """List unit prices per GB for a dataset"""
         try:
